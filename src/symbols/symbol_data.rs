6ca5fd28bd7d548bc81771f3319c6052f081b15c
orxfun <orx.ugur.arikan@gmail.com>
2025-10-23 17:23:40 +0200

    SymbolDataCollection::push is implemented

--- src/symbols/symbol_data.rs
+++ src/symbols/symbol_data.rs
@@ -1,4 +1,7 @@
-use crate::symbols::symbol::Symbol;
+use crate::{
+    model::Model,
+    symbols::{symbol::Symbol, symbol_ref::SymbolRef},
+};
 use orx_imp_vec::ImpVec;
 
 pub struct SymbolDataCollection<S: Symbol> {
@@ -6,7 +9,8 @@ pub struct SymbolDataCollection<S: Symbol> {
 }
 
 impl<S: Symbol> SymbolDataCollection<S> {
-    pub fn push(&self, data: S::Data) {
-        //
+    pub fn push<'m>(&'m self, model: &'m Model, data: S::Data) -> S::Ref<'m> {
+        let data_ref = self.data_vec.imp_push_get_ref(data);
+        SymbolRef::new(model, data_ref).into()
     }
 }
