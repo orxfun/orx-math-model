e587970ec679c8b8051b608c13f12f632e04206f
orxfun <orx.ugur.arikan@gmail.com>
2025-10-23 17:42:32 +0200

    set data uses Definition

--- src/symbols/sets/set_data.rs
+++ src/symbols/sets/set_data.rs
@@ -1,6 +1,7 @@
+use crate::symbols::definition::Definition;
 use alloc::string::String;
 
 pub struct SetData {
     pub key: String,
-    pub definition: String,
+    pub definition: Definition,
 }
