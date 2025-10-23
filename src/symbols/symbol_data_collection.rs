a4df54ed308840b8caf0ca88154dbffb7775d9b0
orxfun <orx.ugur.arikan@gmail.com>
2025-10-23 17:56:09 +0200

    impl SymbolRef::definition

--- src/symbols/symbol_data_collection.rs
+++ src/symbols/symbol_data_collection.rs
@@ -1,10 +1,7 @@
 use crate::{
     model::Model,
-    symbols::{
-        definition::Definition, symbol::Symbol, symbol_data::SymbolData, symbol_ref::SymbolRef,
-    },
+    symbols::{symbol::Symbol, symbol_data::SymbolData, symbol_ref::SymbolRef},
 };
-use alloc::string::String;
 use orx_imp_vec::ImpVec;
 
 pub struct SymbolDataCollection<S: Symbol> {
@@ -21,7 +18,8 @@ impl<S: Symbol> Default for SymbolDataCollection<S> {
 
 impl<S: Symbol> SymbolDataCollection<S> {
     pub fn push<'m>(&'m self, model: &'m Model, symbol_data: SymbolData<S::Data>) -> S::Ref<'m> {
-        let data_ref = self.data_vec.imp_push_get_ref(symbol_data);
-        SymbolRef::new(model, data_ref).into()
+        let data = self.data_vec.imp_push_get_ref(symbol_data);
+        let symbol_ref = SymbolRef { model, data };
+        symbol_ref.into()
     }
 }
--- src/symbols/symbol_ref.rs
+++ src/symbols/symbol_ref.rs
@@ -1,8 +1,9 @@
 use crate::{model::Model, symbols::symbol_data::SymbolData};
+use alloc::string::String;
 
 pub struct SymbolRef<'m, Data> {
-    model: &'m Model,
-    data: &'m SymbolData<Data>,
+    pub(crate) model: &'m Model,
+    pub(crate) data: &'m SymbolData<Data>,
 }
 
 impl<'m, Data> Clone for SymbolRef<'m, Data> {
@@ -17,7 +18,12 @@ impl<'m, Data> Clone for SymbolRef<'m, Data> {
 impl<'m, Data> Copy for SymbolRef<'m, Data> {}
 
 impl<'m, Data> SymbolRef<'m, Data> {
-    pub fn new(model: &'m Model, data: &'m SymbolData<Data>) -> Self {
-        Self { model, data }
+    // pub(crate) fn new(model: &'m Model, data: &'m SymbolData<Data>) -> Self {
+    //     Self { model, data }
+    // }
+
+    pub fn definition(self, definition: impl Into<String>) -> Self {
+        self.data.definition.set(definition);
+        self
     }
 }
