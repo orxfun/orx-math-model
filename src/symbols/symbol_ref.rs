c2f33e88b5a8447bb337bbcf79ec6e1a348c9740
orxfun <orx.ugur.arikan@gmail.com>
2025-11-01 22:55:19 +0100

    add stage to symbol ref

--- src/symbols/symbol_ref.rs
+++ src/symbols/symbol_ref.rs
@@ -1,11 +1,15 @@
-use crate::symbols::{symbol_meta::SymbolMeta, symbol_ref_core::SymbolRefCore};
+use crate::{
+    stages::Stage,
+    symbols::{symbol_meta::SymbolMeta, symbol_ref_core::SymbolRefCore},
+};
 use alloc::string::String;
 use core::fmt::Debug;
 
-pub trait SymbolRef<'m, M>
+pub trait SymbolRef<'m, S, M>
 where
+    S: Stage,
     M: SymbolMeta,
-    Self: SymbolReq<'m, M>,
+    Self: SymbolReq<'m, S, M>,
     M::Data: 'm,
 {
     type Data;
@@ -13,28 +17,30 @@ where
     // provided
 
     fn key(self, key: impl Into<String>) -> Self {
-        let symbol_ref: SymbolRefCore<'_, _> = self.into();
+        let symbol_ref: SymbolRefCore<'_, _, _> = self.into();
         symbol_ref.key(key).into()
     }
 
     fn definition(self, definition: impl Into<String>) -> Self {
-        let symbol_ref: SymbolRefCore<'_, _> = self.into();
+        let symbol_ref: SymbolRefCore<'_, _, _> = self.into();
         symbol_ref.definition(definition).into()
     }
 }
 
 // required traits from Symbol::Ref
 
-pub trait SymbolReq<'m, M>:
-    From<SymbolRefCore<'m, M>> + Into<SymbolRefCore<'m, M>> + Debug
+pub trait SymbolReq<'m, S, M>:
+    From<SymbolRefCore<'m, S, M>> + Into<SymbolRefCore<'m, S, M>> + Debug
 where
+    S: Stage,
     M: SymbolMeta,
 {
 }
 
-impl<'m, M, X> SymbolReq<'m, M> for X
+impl<'m, S, M, X> SymbolReq<'m, S, M> for X
 where
+    S: Stage,
     M: SymbolMeta,
-    X: From<SymbolRefCore<'m, M>> + Into<SymbolRefCore<'m, M>> + Debug,
+    X: From<SymbolRefCore<'m, S, M>> + Into<SymbolRefCore<'m, S, M>> + Debug,
 {
 }
