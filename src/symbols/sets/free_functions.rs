f290d4bd1e181778468c9aa24bbcccb2a5243a7e
orxfun <orx.ugur.arikan@gmail.com>
2025-11-01 21:00:21 +0100

    clippy fix

--- src/symbols/sets/free_functions.rs
+++ src/symbols/sets/free_functions.rs
@@ -1,9 +1,9 @@
 use crate::symbols::{symbol_ref::SymbolRef, Set};
 
 pub fn set_of<'m, const N: usize>(sets: [Set<'m>; N]) -> Set<'m> {
+    assert_ne!(N, 0);
     // TODO: make sure all sets belong to the same model
     match N {
-        0 => panic!("not good!"),
         _ => {
             let symbol: SymbolRef<'_, _> = sets[0].into();
             symbol.model.dep_set(sets)
