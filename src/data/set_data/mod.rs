dab190fcc202bc1a1d4a7cadef8e68a54860f4a4
orxfun <orx.ugur.arikan@gmail.com>
2025-11-06 13:16:07 +0100

    d1 test compiling!

--- src/data/set_data/mod.rs
+++ src/data/set_data/mod.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+mod tests;
+
 mod set_and_data;
 mod set_data_dim1;
 mod set_gen;
---
+++ src/data/set_data/tests/set_data_dim1.rs
@@ -0,0 +1,16 @@
+use crate::*;
+use alloc::vec;
+use alloc::vec::Vec;
+
+#[test]
+fn set_data_dim1_jagged() {
+    // model
+
+    let m = Model::new();
+
+    let i = m.set();
+    let j = set_of([i]);
+
+    let data1 = vec![vec![], vec![1, 2, 3], vec![3, 5]];
+    let dj = j.data(&data1, |d, i| d[i].iter().copied());
+}
