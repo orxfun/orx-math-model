2d8bb5028c84d0842f9d1f999fbe1f20b06cda4a
orxfun <orx.ugur.arikan@gmail.com>
2025-11-07 18:15:55 +0100

    wip tuple to array conversion

--- src/wip.rs
+++ src/wip.rs
@@ -1,4 +1,7 @@
-use crate::symbols::sets::SetCore;
+use crate::{
+    symbols::{pars::ParData, sets::SetCore, DependentSetIndices, Symbol},
+    Model, Par, Set,
+};
 use core::marker::PhantomData;
 
 // trait
@@ -11,6 +14,8 @@ pub trait StQueue<'m> {
     type Front: Into<SetCore<'m>>;
 
     type Back: StQueue<'m>;
+
+    const LEN: usize;
 
     fn push<Elem>(self, element: Elem) -> Self::PushBack<Elem>
     where
@@ -39,6 +44,7 @@ where
     type Front = F;
 
     type Back = Self;
+    const LEN: usize = 1;
 
     fn push<Elem>(self, element: Elem) -> Self::PushBack<Elem>
     where
@@ -80,6 +86,7 @@ where
     type Front = F;
 
     type Back = B;
+    const LEN: usize = 1 + B::LEN;
 
     fn push<Elem>(self, element: Elem) -> Self::PushBack<Elem>
     where
@@ -91,4 +98,72 @@ where
             p: PhantomData,
         }
     }
+}
+
+// tuple
+
+impl<'m, X1> From<X1> for QueueSingle<'m, X1>
+where
+    X1: Into<SetCore<'m>>,
+{
+    fn from(value: X1) -> Self {
+        QueueSingle {
+            f: value,
+            p: PhantomData,
+        }
+    }
+}
+
+impl<'m, X1, X2> From<(X1, X2)> for QueueMulti<'m, X1, QueueSingle<'m, X2>>
+where
+    X1: Into<SetCore<'m>>,
+    X2: Into<SetCore<'m>>,
+{
+    fn from((x1, x2): (X1, X2)) -> Self {
+        QueueSingle::from(x1).push(x2)
+    }
+}
+
+impl Model {
+    pub(crate) fn par2<'m, const N: usize, Q, T>(&'m self, sets: T) -> Par<'m, N>
+    where
+        Q: StQueue<'m>,
+        T: Into<Q>,
+    {
+        // let dep = DependentSetIndices::new(sets.into_iter());
+        // let data = ParData::new(dep);
+        // self.data.pars.push(self, Symbol::new(data)).with_dim()
+        todo!()
+    }
+}
+
+// tuple 2
+
+impl<'m, const N: usize> From<Set<'m, N>> for [SetCore<'m>; 1] {
+    fn from(x1: Set<'m, N>) -> Self {
+        [x1.into()]
+    }
+}
+
+#[test]
+fn abc() {
+    use super::*;
+    use crate::*;
+
+    let m = Model::new();
+    let i = m.set();
+    let j = set_of([i]);
+
+    let sets = QueueSingle {
+        f: i,
+        p: PhantomData,
+    };
+    type IJ<'m> = QueueMulti<'m, Set<'m>, QueueSingle<'m, Set<'m, 1>>>;
+
+    let sets: IJ = sets.push(j);
+
+    let sets: IJ = (i, j).into();
+    // m.par2::<2, QueueMulti<'_, Set<'_>, QueueMulti<'_, Set<'_>, QueueSingle<'_, Set<'_, 1>>>>, (Set<'_>, Set<'_, 1>)>((i, j));
+
+    m.par2::<2, IJ, _>((i, j));
 }
