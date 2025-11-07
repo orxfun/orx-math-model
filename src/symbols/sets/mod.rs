f1bb75e02bd173317f1e975fa8053883c03b58e9
orxfun <orx.ugur.arikan@gmail.com>
2025-11-07 20:03:25 +0100

    rename set collection module

--- src/symbols/sets/mod.rs
+++ src/symbols/sets/mod.rs
@@ -3,13 +3,13 @@ mod tests;
 
 mod free_functions;
 mod r#ref;
+mod set_collection;
 mod set_data;
 mod set_meta;
-mod set_tuples;
 mod subsets;
 
 pub use free_functions::set_of;
 pub use r#ref::{Set, SetCore};
+pub use set_collection::SetCollection;
 pub use set_data::SetData;
 pub use set_meta::SetMeta;
-pub use set_tuples::SetCollection;
