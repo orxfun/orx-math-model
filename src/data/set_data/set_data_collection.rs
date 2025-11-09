871220233a69d75b4a8793a485956f397488703a
orxfun <orx.ugur.arikan@gmail.com>
2025-11-09 22:20:27 +0100

    define and implement SetDataCollection

--- src/data/set_data/set_data_collection.rs
+++ src/data/set_data/set_data_collection.rs
@@ -1,7 +1,28 @@
-use crate::symbols::pars::ParCore;
-use crate::{symbols::sets::SetCore, Set};
-use crate::{Model, Par, SetAndData};
+use crate::SetAndData;
+use alloc::boxed::Box;
 
 pub trait SetDataCollection<'m> {
-    // fn into_iter(self) -> impl Iterator<Item = SetAndData<'m>>;
+    fn into_iter(self) -> impl IntoIterator<Item = Box<dyn SetAndData<'m> + 'm>>;
+}
+
+impl<'m, S1> SetDataCollection<'m> for S1
+where
+    S1: SetAndData<'m> + 'm,
+{
+    fn into_iter(self) -> impl IntoIterator<Item = Box<dyn SetAndData<'m> + 'm>> {
+        let s0: Box<dyn SetAndData<'m> + 'm> = Box::new(self);
+        core::iter::once(s0)
+    }
+}
+
+impl<'m, S1, S2> SetDataCollection<'m> for (S1, S2)
+where
+    S1: SetAndData<'m> + 'm,
+    S2: SetAndData<'m> + 'm,
+{
+    fn into_iter(self) -> impl IntoIterator<Item = Box<dyn SetAndData<'m> + 'm>> {
+        let s0: Box<dyn SetAndData<'m> + 'm> = Box::new(self.0);
+        let s1: Box<dyn SetAndData<'m> + 'm> = Box::new(self.1);
+        [s0, s1]
+    }
 }
