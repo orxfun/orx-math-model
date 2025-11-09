87b4d9a5c6ccc8242260822df342ac2702a7c654
orxfun <orx.ugur.arikan@gmail.com>
2025-11-09 21:22:17 +0100

    init elements for generic FunSet

--- src/data/set_data/fun_set.rs
+++ src/data/set_data/fun_set.rs
@@ -1,3 +1,7 @@
+use crate::data::set_data::indices::{IndexValues, SetDepths};
+use crate::symbols::sets::SetCore;
+use crate::SetGen;
+use alloc::boxed::Box;
 use orx_self_or::SoR;
 
 pub struct FunSet<'d, Data, I, T, F>
@@ -30,5 +34,24 @@ where
     ) -> FunSet<'d, Data, I, T, impl Fn(&'d Data, &[usize]) -> I> {
         let fun = move |data: &'d Data, indices: &[usize]| fun(data, indices[0]);
         FunSet { data, fun }
+    }
+}
+
+impl<'d, 'm, Data, I, T, F> SetGen<'m> for FunSet<'d, Data, I, T, F>
+where
+    I: IntoIterator<Item = T>,
+    T: SoR<usize>,
+    F: Fn(&'d Data, &[usize]) -> I,
+{
+    fn elements(
+        &self,
+        set: SetCore<'m>,
+        depths: &SetDepths<'m>,
+        index_values: &IndexValues,
+    ) -> Box<dyn Iterator<Item = usize> + '_> {
+        let dep_sets = set.depending_sets_core();
+        let depths = dep_sets.map(|s| depths.depth_of(s));
+        let indices = depths.map(|d| index_values[d]);
+        todo!()
     }
 }
