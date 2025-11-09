bb6bbed70138301234680abd18d817fe1ff742af
orxfun <orx.ugur.arikan@gmail.com>
2025-11-09 22:00:46 +0100

    clean up

--- src/symbols/sets/ref/set_with_values.rs
+++ src/symbols/sets/ref/set_with_values.rs
@@ -4,18 +4,6 @@ use crate::Set;
 use orx_self_or::SoR;
 
 impl<'m> Set<'m, 0> {
-    // pub fn data<'d, Data, I, T>(
-    //     self,
-    //     data: &'d Data,
-    //     fun: fn(&'d Data) -> I,
-    // ) -> FunSetAndDataD0<'m, 'd, Data, I, T>
-    // where
-    //     I: IntoIterator<Item = T>,
-    //     T: SoR<usize>,
-    // {
-    //     FunSetAndDataD0::new(self, data, fun)
-    // }
-
     pub fn data<'d, Data, I, T>(
         self,
         data: &'d Data,
@@ -38,18 +26,6 @@ impl<'m> Set<'m, 0> {
 }
 
 impl<'m> Set<'m, 1> {
-    // pub fn data<'d, Data, I, T>(
-    //     self,
-    //     data: &'d Data,
-    //     fun: fn(&'d Data, usize) -> I,
-    // ) -> FunSetAndDataD1<'m, 'd, Data, I, T>
-    // where
-    //     I: IntoIterator<Item = T>,
-    //     T: SoR<usize>,
-    // {
-    //     FunSetAndDataD1::new(self, data, fun)
-    // }
-
     pub fn data<'d, Data, I, T>(
         self,
         data: &'d Data,
