b8f0c1615a134dc6dddd3739cef00a36e50db54d
orxfun <orx.ugur.arikan@gmail.com>
2025-11-12 03:33:58 +0100

    implement Par::data for dim-2 parameters

--- src/symbols/pars/ref/par_to_par_data.rs
+++ src/symbols/pars/ref/par_to_par_data.rs
@@ -39,8 +39,32 @@ impl<'m> Par<'m, 1> {
 
         let fun = move |data: &'d Data, indices: &[usize]| {
             debug_assert_eq!(indices.len(), 1);
-            let i1 = indices[0];
-            fun(data, i1)
+            let i0 = indices[0];
+            fun(data, i0)
+        };
+
+        FunParData::new(par, data, fun)
+    }
+}
+
+impl<'m> Par<'m, 2> {
+    pub fn data<'d, Data, N, T, F>(
+        self,
+        data: &'d Data,
+        fun: F,
+    ) -> FunParData<'d, 'm, Data, N, T, impl Fn(&'d Data, &[usize]) -> T>
+    where
+        N: Number,
+        T: SoR<N>,
+        F: Fn(&'d Data, usize, usize) -> T,
+    {
+        let par = ParCore::from(self);
+
+        let fun = move |data: &'d Data, indices: &[usize]| {
+            debug_assert_eq!(indices.len(), 2);
+            let i0 = indices[0];
+            let i1 = indices[1];
+            fun(data, i0, i1)
         };
 
         FunParData::new(par, data, fun)
