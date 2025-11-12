2a52c082e01b81e68a5a13b329bea2a8f39c3b4f
orxfun <orx.ugur.arikan@gmail.com>
2025-11-12 03:39:09 +0100

    create par data tests for dimension 0

--- src/data/par_data/tests/par_data_dim0.rs
+++ src/data/par_data/tests/par_data_dim0.rs
@@ -1,14 +1,37 @@
-use crate::data::SetAndData;
+use crate::data::par_data::par_and_data::ParAndData;
 use crate::Model;
 use alloc::vec;
 use alloc::vec::Vec;
 
 #[test]
-fn set_data_dim0_range() {
+fn par_data_dim0_independent() {
     let m = Model::new();
 
     let s = m.par();
 
-    // let values: Vec<_> = di.elements(&set_depths, &index_values).collect();
-    // assert_eq!(values, vec![3, 4, 5, 6]);
+    let ds = s.data(&(), |_| 42);
+    assert_eq!(ds.value(&[]), 42.0);
+
+    let ds = s.data(&(), |_| 12f32);
+    assert_eq!(ds.value(&[]), 12.0);
+}
+
+#[test]
+fn par_data_dim0_from_data() {
+    let m = Model::new();
+
+    let s = m.par();
+
+    let data = 42;
+    let ds = s.data(&data, |d| *d);
+    assert_eq!(ds.value(&[]), 42.0);
+
+    struct Data {
+        things: Vec<usize>,
+    }
+    let data = Data {
+        things: vec![0, 1, 2, 3],
+    };
+    let ds = s.data(&data, |d| d.things.len());
+    assert_eq!(ds.value(&[]), 4.0);
 }
