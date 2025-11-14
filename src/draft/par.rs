c82116dbbed746a0ba23315721b61d0ed1eab4a1
orxfun <orx.ugur.arikan@gmail.com>
2025-11-14 22:38:55 +0100

    introduce ParRef

--- src/draft/par.rs
+++ src/draft/par.rs
@@ -1,5 +1,12 @@
-use crate::draft::set::*;
-use core::ops::Index;
+use crate::draft::{scalar::Scalar, set::*};
+use core::{marker::PhantomData, ops::Index};
+
+pub struct ParRef<P: Par>(PhantomData<P>);
+impl<P: Par> ParRef<P> {
+    pub fn new() -> Self {
+        Self(PhantomData)
+    }
+}
 
 pub trait Par {
     type Value;
@@ -7,11 +14,15 @@ pub trait Par {
 
 pub trait Par0: Par {}
 
-pub trait Par1: Par
-where
-    Self: Index<<Self::S0 as Set>::Key>,
-{
+pub trait Par1: Par {
     type S0: Set0;
 
     fn value(&self, element: &<Self::S0 as Set>::Elem) -> Self::Value;
+}
+
+impl<P: Par1> Index<<P::S0 as Set>::Key> for ParRef<P> {
+    type Output = Scalar;
+    fn index(&self, _: <P::S0 as Set>::Key) -> &Self::Output {
+        &Scalar
+    }
 }
