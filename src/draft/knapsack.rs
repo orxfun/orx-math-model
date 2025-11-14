f899774b8d30a9b0a07ee642c4df5ffc01e1eb11
orxfun <orx.ugur.arikan@gmail.com>
2025-11-14 22:09:50 +0100

    define Items set for knapsack with str keys

--- src/draft/knapsack.rs
+++ src/draft/knapsack.rs
@@ -1,7 +1,17 @@
-pub struct Item {
+use crate::draft::set::*;
+struct Item {
     name: &'static str,
     weight: u64,
     value: f32,
 }
 
-pub struct Knapsack {}
+struct Items;
+impl Set for Items {
+    type Key = &'static str;
+    type Elem = Item;
+}
+impl Set0 for Items {}
+
+fn model() {
+    let items = Items;
+}
